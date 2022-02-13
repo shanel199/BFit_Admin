420ce1b0ba56c364c869297eeebde11d344560f5
shanel199 <shanalfrancin@gmail.com>
2022-02-14 01:27:55 +0530

    Logout added

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,11 +1,9 @@
 
 k
 ;app/src/main/java/com/shcollege/bfitadmin/SplashScreen.java,9\c\9ce8c71d808c8b75e1f5c356e9e288778e8370ce
-@
-app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 H
 app/google-services.json,f\d\fd8d34b142d02ec743a8efefefd2c097961666e9
 i
 9app/src/main/java/com/shcollege/bfitadmin/GetStarted.java,f\d\fd764a69e87469ef47f369f8048fdf4037c2344e
+c
+3app/src/main/java/com/shcollege/bfitadmin/User.java,3\e\3eceeff6cbfdf3de4a254591507a7575881d97fb
--- app/src/main/java/com/shcollege/bfitadmin/HomeActivity.java
+++ app/src/main/java/com/shcollege/bfitadmin/HomeActivity.java
@@ -2,13 +2,44 @@ package com.shcollege.bfitadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private TextView logoutbtn;
+    private ImageView exercise_iv,equipment_iv,diet_iv,bodypart_iv,workout_iv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        logoutbtn = (TextView) findViewById(R.id.logoutbtn);
+        logoutbtn.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+            switch (v.getId()) {
+                case R.id.logoutbtn:
+                    logout();
+                    break;
+        }
+
+
 }
+    private void logout() {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            finish();
+
+    }
+    }
